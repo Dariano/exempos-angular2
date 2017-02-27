bed885fab0ce2ee154f8cbf2c35a51bddb8af1c4
dariano <darianosoares@hotmail.com>
2017-02-27 14:56:20 -0300

    Adicionado angular2-materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { ComponenteCriadoPeloNgComponent } from './componente-criado-pelo-ng/componente-criado-pelo-ng.component';
 import { CursosComponent } from './cursos/cursos.component';
 
+
 import { CursosModule } from './cursos/cursos.module';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 import { HighlightDirective } from './property-binding/input-property.component';
-import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+
+import { MaterializeModule } from 'angular2-materialize';
+import "angular2-materialize";
 
 @NgModule({
   declarations: [
@@ -20,13 +23,13 @@ import { LifeCycleComponent } from './life-cycle/life-cycle.component';
     MeuPrimeiroComponent,
     ComponenteCriadoPeloNgComponent,
     PropertyBindingComponent,
-    HighlightDirective,
-    LifeCycleComponent
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterializeModule,
     CursosModule
   ],
   providers: [],
